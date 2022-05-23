c115b8f1fb138274a92368204f6e7bd6c50ae867
khaoula Naboussi <k12.naboussi@gmail.com>
2022-05-23 16:04:00 +0100

    working on database

--- user_management_backend/src/app.module.ts
+++ user_management_backend/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IntraAuthModule } from './42-auth/IntraAuth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configService } from './config/config.service';
 
 @Module({
-  imports: [IntraAuthModule],
+  imports: [
+    IntraAuthModule,
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
