0319a040ab359f30a445563ee326dbf5a113a842
khaoula NABOUSSI <k12.naboussi@gmail.com>
2022-06-07 19:22:02 +0100

    working on the login page, need to add additinal design in the login box, nest step the logic

--- user_management_frontend/src/login/login.js
+++ user_management_frontend/src/login/login.js
@@ -1,4 +1,5 @@
-import { useRef , useState, useEffect } from 'react'
+import { useState } from 'react'
+import './login.css'
 
 const Login = () => {
     const adminUser = {
@@ -20,7 +21,32 @@ const Login = () => {
 
     return (
         <>
-            <button className=''></button>
+            <div className="card">
+                <div className='contributorBox'>
+                    <h1>
+                        PONG 
+                        CLASSIC
+                    </h1>
+                    <div class="field">
+                        <div class="ping"></div>
+                        <div className="pad"></div>
+	                    <div class="pong"></div>
+	                    <div class="ball"></div>
+                    </div>
+                    <h6>
+                        Designed and developed with <i className="fa-solid fa-heart"></i> by:
+                    </h6>
+                    <div className='dev'>
+                        <a href='https://github.com/NAB-khaoula' className='devLink'>ME</a>
+                        <a href='https://github.com/mojahid-belaman' className='devLink'>MB</a>
+                        <a href='#' className='devLink'>SH</a>
+                        <a href='#' className='devLink'>AB</a>
+                    </div>
+                </div>
+                <div className="LoginBox">
+                    <button className='LoginButton'>Login With 42 Intra</button>
+                </div>
+            </div>
         </>
     )
 }
